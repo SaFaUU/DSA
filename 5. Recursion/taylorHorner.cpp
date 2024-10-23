274a086bfab4e62d6cb7e861e9e48e140872d61b
Safa Asgar <safaasgar58@gmail.com>
2024-10-23 12:23:50 +0600

    Taylor Series using Horners Rule

---
+++ 5. Recursion/taylorHorner.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+double taylorHorner(double x, double n)
+{
+    static double f = 1;
+    static double r = 1;
+
+    if (n == 0)
+    {
+        return r;
+    }
+    r = 1 + (x / n) * r;
+    return taylorHorner(x, n - 1);
+}
+
+int main()
+{
+    printf("%lf ", taylorHorner(1, 10));
+    return 0;
+}
