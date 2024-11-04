6206465ad0087159e65f50d2263253549094b8cf
Safa Asgar <safaasgar58@gmail.com>
2024-11-04 23:22:11 +0600

    Working with 2D Arrays

--- 6. Arrays Representations/array2D.cpp
+++ 6. Arrays Representations/array2D.cpp
@@ -17,11 +17,36 @@ int main()
     cout << "Matrix 2:" << endl;
 
     int *B[3];
-    B[0] = new int[4];
-    B[1] = new int[4];
-    B[2] = new int[4];
+    B[0] = new int[4]{1, 2, 3, 4};
+    B[1] = new int[4]{4, 5, 6, 7};
+    B[2] = new int[4]{7, 8, 9, 10};
 
-    B[0] = {1, 2, 3, 4};
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            cout << B[i][j] << " ";
+        }
+        cout << endl;
+    };
+
+    int **C;
+    C = new int *[3];
+
+    C[0] = new int[4]{1, 2, 3, 4};
+    C[1] = new int[4]{4, 5, 6, 7};
+    C[2] = new int[4]{7, 8, 9, 10};
+
+    cout << "Matrix 3:" << endl;
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            cout << C[i][j] << " ";
+        }
+        cout << endl;
+    };
 
     return 0;
 }
